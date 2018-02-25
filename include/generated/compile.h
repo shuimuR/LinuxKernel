cd2727eb47fa2415f37ee67ba8563be83e859cef
shuimu <razhgu@sina.com>
2018-02-25 16:07:50 +0800

    build the git repo, achieve uart output

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 2 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 Tue Feb 13 08:38:52 CST 2018"
+#define LINUX_COMPILE_BY "shuimu"
+#define LINUX_COMPILE_HOST "shuimur"
+#define LINUX_COMPILER "gcc version 4.5.1 (ctng-1.8.1-FA)"
